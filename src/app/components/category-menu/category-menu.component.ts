175d7988f69a293c6fd7ef876fa7778c09ae0f68
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-10-29 10:41:40 -0500

    Dynamic menu

---
+++ src/app/components/category-menu/category-menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-category-menu',
+  templateUrl: './category-menu.component.html',
+  styleUrls: ['./category-menu.component.css']
+})
+export class CategoryMenuComponent implements OnInit {
+  productCategories: ProductCategory[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+    this.productService.getProducCategories().subscribe(
+      data => {
+        console.log('Product Categories=' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    );
+  }
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,26 +3,43 @@ import { Injectable } from '@angular/core';
 import { Product } from '../common/product';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpCliente: HttpClient) { }
 
+  //Products
   getProductList(theCategoryId: number): Observable<Product[]>{
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpCliente.get<GetResponse>(searchUrl).pipe(
+    return this.httpCliente.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  //Product categories
+  getProducCategories() : Observable<ProductCategory[]> {
+    return this.httpCliente.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse { 
+interface GetResponseProducts { 
   _embedded: {
       products: Product[];
+    }
+}
+
+interface GetResponseProductCategory { 
+  _embedded: {
+      productCategory: ProductCategory[];
     }
 }
