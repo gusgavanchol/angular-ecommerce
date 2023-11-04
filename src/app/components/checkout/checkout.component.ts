efcb93b00f3bfdc55bfccd799a0116d7856c3fac
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-11-04 17:46:41 -0500

    Secure backend

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -158,15 +158,6 @@ export class CheckoutComponent implements OnInit {
     // get cart items
     const cartItems = this.cartService.cartItems;
 
-    // create orderItems from cartItems
-    // - long way
-    /*
-    let orderItems: OrderItem[] = [];
-    for (let i=0; i < cartItems.length; i++) {
-      orderItems[i] = new OrderItem(cartItems[i]);
-    }
-    */
-
     // - short way of doing the same thingy
     let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
 
@@ -216,7 +207,6 @@ export class CheckoutComponent implements OnInit {
     this.cartService.totalQuantity.next(0);
 
     this.checkoutFormGroup.reset();
-
     this.router.navigateByUrl("/products");
   }
 
