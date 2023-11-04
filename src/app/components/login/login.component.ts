0915cd46bc3f32a88faa377542b31bb26a75c7e6
gustavo.gavancho_dev <gustavo.gavancho@devsu.com>
2023-11-03 21:47:44 -0500

    Login Part II

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,19 +2,20 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import OktaSignIn from '@okta/okta-signin-widget';
-
 import myAppConfig from 'src/app/config/my-app-config';
-
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  oktaSignin: any;
-
+export class LoginComponent implements OnInit{
+ 
+  oktaSignin: any; 
+ 
   constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
-    this.oktaSignin = new OktaSignIn({ 
+ 
+    this.oktaSignin = new OktaSignIn({
       logo: 'assets/images/logo.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
@@ -26,21 +27,21 @@ export class LoginComponent implements OnInit {
       }
     });
   }
-  
+ 
   ngOnInit(): void {
     this.oktaSignin.remove();
-
+ 
     this.oktaSignin.renderEl({
-      el: '#okta-sign-in-widget'}, 
+      el: '#okta-sign-in-widget'},
       (response: any) => {
-        if(response.status === 'SUCCESS'){
+        if (response.status === 'SUCCESS') {
           this.oktaAuth.signInWithRedirect();
         }
       },
       (error: any) => {
         throw error;
       }
-      )
+      );
   }
-
+ 
 }
